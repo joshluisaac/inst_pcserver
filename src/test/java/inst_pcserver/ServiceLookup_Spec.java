668817d3c452f90a9ad68c20799f69c8dc0c3268
Josh Uzo <nwankwo.joshua@gmail.com>
2019-04-20 17:59:55 +1000

    Updated specs and tests

--- src/test/java/inst_pcserver/ServiceLookup_Spec.java
+++ src/test/java/inst_pcserver/ServiceLookup_Spec.java
@@ -2,26 +2,44 @@ package inst_pcserver;
 
 import java.util.Arrays;
 import java.util.Properties;
-
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-
 import com.avantsystems.ServiceLookup;
 import com.profitera.util.Utilities;
 
 public class ServiceLookup_Spec {
-	
-	@Test
-	public void loadProp() {
-		Properties prop = Utilities.loadOrExit("BusinessServices.properties");
-		
-		ServiceLookup serviceLookup  = new ServiceLookup(prop);
-		
-		//get all services
-		String[] allServices = serviceLookup.getAllServiceNames();
 
-		serviceLookup.getFailedServices();
-		
-		//serviceLookup.getService(name);
-	}
+
+
+    /* dependencies */
+    ServiceLookup serviceLookup;
+    Properties prop;
+
+    @Before
+    public void run_once_per_test() {
+        prop = Utilities.loadOrExit("BusinessServices.properties");
+        serviceLookup = new ServiceLookup(prop);
+    }
+
+
+
+    // @Test
+    public void loadProp() {
+
+        // get all services
+        String[] allServices = serviceLookup.getAllServiceNames();
+
+    }
+
+    @Test
+    public void expects_failed_service_when_invalid_service_is_passed() {
+        boolean result = serviceLookup.getFailedServices()
+                .contains("com.avantsystems.business.services.ErrorService");
+        Assert.assertThat(result, CoreMatchers.is(true));
+    }
+
+
 
 }
