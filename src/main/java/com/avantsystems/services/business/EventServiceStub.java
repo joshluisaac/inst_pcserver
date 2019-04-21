aa40afb137f860889b0c1788050fed2d9329f989
Josh Uzo <nwankwo.joshua@gmail.com>
2019-04-21 17:02:58 +1000

    Package refactoring

--- src/main/java/com/avantsystems/services/business/EventServiceStub.java
+++ src/main/java/com/avantsystems/services/business/EventServiceStub.java
@@ -1,11 +1,12 @@
 package com.avantsystems.services.business;
 
+import java.util.HashMap;
 import java.util.Map;
-import com.avantsystems.ServiceLookup;
-import com.avantsystems.ServiceLookupManager;
-import com.avantsystems.ServiceType;
 import com.avantsystems.services.IEventService;
 import com.avantsystems.services.IScheduleListener;
+import com.avantsystems.services.ServiceLookup;
+import com.avantsystems.services.ServiceLookupManager;
+import com.avantsystems.services.ServiceType;
 import com.avantsystems.services.TransferObject;
 import com.avantsystems.services.business.http.IMessageHandler;
 
@@ -22,9 +23,9 @@ public class EventServiceStub extends BusinessService implements IEventService, 
     public TransferObject sendEvent(String name, Map<String, Object> arguments) {
        ServiceLookup system = ServiceLookupManager.getInstance().getServiceLookup(ServiceType.SYSTEM_SERVICES);
        IMessageHandler stub = (IMessageHandler) system.getService("MessageHandler");
-        return null;
+       Object result = stub.handleMessage(this, "EventService", "sendEvent", new Class<?>[]{String.class, Map.class},
+               new Object[]{name, arguments}, new HashMap<String, Object>());
+           return (TransferObject) result;
     }
-    
-    
 
 }
